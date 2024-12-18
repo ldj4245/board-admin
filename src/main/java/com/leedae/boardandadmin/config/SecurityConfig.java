aeaa0fe220ad62416ae9d2cbade5a961b71ac431
idaejin <ldj4241@naver.com>
2024-12-18 22:01:09 +0900

    #1 - 스프링 부트 프로젝트 초기화
    스프링부트 뼈대 작성

---
+++ src/main/java/com/leedae/boardandadmin/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.leedae.boardandadmin.config;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(withDefaults())
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .build();
+    }
+
+
+
+}
