8fc60bfe6d5d92a314fc04d56bf5f666c89b424b
idaejin <ldj4241@naver.com>
2024-12-20 00:28:31 +0900

    #2- 도메인 설계 : 회원, 권한 enum, converter

---
+++ src/main/java/com/leedae/boardandadmin/domain/UserAccount.java
@@ -0,0 +1,86 @@
+package com.leedae.boardandadmin.domain;
+
+import com.leedae.boardandadmin.domain.constant.RoleType;
+import com.leedae.boardandadmin.domain.converter.RoleTypesConverter;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString(callSuper = true)
+@Table(indexes = {
+        @Index(columnList = "email", unique = true),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+public class UserAccount extends AuditingFields {
+
+    @Id
+    @Column(length = 50)
+    private String userId;
+
+    @Setter @Column(nullable = false) private String userPassword;
+
+    @Convert(converter = RoleTypesConverter.class)
+    @Column(nullable = false)
+    private Set<RoleType> roleTypes = new LinkedHashSet<>();
+
+    @Setter @Column(length = 100) private String email;
+    @Setter @Column(length = 100) private String nickname;
+    @Setter private String memo;
+
+
+    protected UserAccount() {}
+
+    private UserAccount(String userId, String userPassword, Set<RoleType> roleTypes, String email, String nickname, String memo, String createdBy) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.roleTypes = roleTypes;
+        this.email = email;
+        this.nickname = nickname;
+        this.memo = memo;
+        this.createdBy = createdBy;
+        this.modifiedBy = createdBy;
+    }
+
+
+    public static UserAccount of(String userId, String userPassword,Set<RoleType> roletypes, String email, String nickname, String memo) {
+        return new UserAccount(userId, userPassword,roletypes, email, nickname, memo,null);
+    }
+
+
+    public static UserAccount of(String userId, String userPassword,Set<RoleType> roleTypes, String email, String nickname, String memo, String createdBy) {
+        return new UserAccount(userId, userPassword,roleTypes, email, nickname, memo,createdBy);
+    }
+
+    public void addRoleType(RoleType roleType) {
+        this.getRoleTypes().add(roleType);
+    }
+
+    public void addRoleType(Collection<RoleType> roleTypes) {
+        this.getRoleTypes().addAll(roleTypes);
+    }
+
+    public  void removeRoleType(RoleType roleType) {
+        this.getRoleTypes().remove(roleType);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof UserAccount that)) return false;
+        return Objects.equals(getUserId(), that.getUserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getUserId());
+    }
+}
