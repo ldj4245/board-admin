420c46d3a0b9bbfadc766eaca60767023aa69435
idaejin <ldj4241@naver.com>
2025-01-06 22:52:02 +0900

    #24 - refactor : ArticleDto 오타 수정

--- src/main/java/com/leedae/boardandadmin/dto/ArticleDto.java
+++ src/main/java/com/leedae/boardandadmin/dto/ArticleDto.java
@@ -8,15 +8,15 @@ public record ArticleDto(
         UserAccountDto userAccount,
         String title,
         String content,
-        Set<String> hahstags,
+        Set<String> hashtags,
         LocalDateTime createdAt,
         String createdBy,
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
 
-    public static  ArticleDto of(Long id, UserAccountDto userAccount, String title, String content, Set<String> hahstags, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new ArticleDto(id, userAccount, title, content, hahstags, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static  ArticleDto of(Long id, UserAccountDto userAccount, String title, String content, Set<String> hashtags, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleDto(id, userAccount, title, content, hashtags, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
 
